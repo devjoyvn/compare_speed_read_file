1c31ba20042c44dc875d62084475a95fdf531069
klongg <kieulong1302@gmail.com>
2022-09-12 17:02:19 +0700

    “init”

---
+++ FileInputStream.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class FileInputStream {
+    /**
+     *  Param: pathname to readfile
+     *  Return: time execution: milis
+     */
+    public static long read(String pathName) {
+        long start = System.currentTimeMillis();
+
+        try (java.io.FileInputStream fin = new java.io.FileInputStream(pathName)) {
+            int data = fin.read();
+            while (data != -1) {
+                data = fin.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+        return end - start;
+    }
+}
